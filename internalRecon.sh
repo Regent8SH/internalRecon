a52a31fe12f3455f6ebe1a62ba74fc14e6513aa0
Regen Peterson <regent8sh@gmail.com>
2023-12-05 22:04:07 +0000

    Update internalRecon.sh
    Added check for scope file and instructional message to user if missing.

--- internalRecon.sh
+++ internalRecon.sh
@@ -2,7 +2,6 @@
 
 ## internalRecon.sh
 ## by Regen Peterson
-## Make sure CME is installed
 
 while getopts f:t:h: flag
 do
@@ -11,23 +10,29 @@ do
     esac
 done
 
-wd=$(pwd)
-echo "Creating recon directory at $wd/Recon"
-mkdir Recon
+if [ -z $scopeFile ]
+then 
+    echo "Please include the scope file with './internalRecon.sh -f {FILENAME}'"
 
-echo "Using CrackMapExec to generate relay target list at $wd/Recon/relayTargets.txt"
-crackmapexec smb $scopeFile --gen-relay-list Recon/relayTargets.txt | tee Recon/cmeOut.txt
+else 
+    wd=$(pwd)
+    echo "Creating recon directory at $wd/Recon"
+    mkdir Recon
 
-echo "Extracting domain info from CME output and saving identified domainNames at $wd/Recon/domainNames.txt"
-cat Recon/cmeOut.txt | grep -i 'domain:' | awk -F'omain:' '{print $2}' | cut -d ')' -f 1 | sort -u > Recon/domainNames.txt
+    echo "Using CrackMapExec to generate relay target list at $wd/Recon/relayTargets.txt"
+    crackmapexec smb $scopeFile --gen-relay-list Recon/relayTargets.txt | tee Recon/cmeOut.txt
 
-echo "Identifying DC Hostnames via nslookup and saving at $wd/Recon/dcHostnames.txt"
-while read p; do
-  nslookup -type=srv _ldap._tcp.dc._msdcs.$p | grep -i '.'$p | grep -vi 'failed:'| cut -d ' ' -f 6 | sed 's/.$//' >> Recon/dcHostnames.txt
-done < Recon/domainNames.txt
+    echo "Extracting domain info from CME output and saving identified domainNames at $wd/Recon/domainNames.txt"
+    cat Recon/cmeOut.txt | grep -i 'domain:' | awk -F'omain:' '{print $2}' | cut -d ')' -f 1 | sort -u > Recon/domainNames.txt
 
-echo "Extracing DC IP addresses with ping and saving to $wd/Recon/dcIpAddresses.txt"
-for ip in $(cat Recon/dcHostnames.txt); do ping $ip -c 1 | grep -i '64 bytes' | cut -d '(' -f 2 | cut -d ')' -f 1 >> Recon/dcIpAddresses.txt; done
+    echo "Identifying DC Hostnames via nslookup and saving at $wd/Recon/dcHostnames.txt"
+    while read p; do
+      nslookup -type=srv _ldap._tcp.dc._msdcs.$p | grep -i '.'$p | grep -vi 'failed:'| cut -d ' ' -f 6 | sed 's/.$//' >> Recon/dcHostnames.txt
+    done < Recon/domainNames.txt
 
-echo 'Checking DCs for LDAP Signing? (Y/N): '
-crackmapexec ldap Recon/dcIpAddresses.txt -M ldap-signing
+    echo "Extracting DC IP addresses with ping and saving to $wd/Recon/dcIpAddresses.txt"
+    for ip in $(cat Recon/dcHostnames.txt); do ping $ip -c 1 | grep -i '64 bytes' | cut -d '(' -f 2 | cut -d ')' -f 1 >> Recon/dcIpAddresses.txt; done
+
+    echo 'Checking DCs for LDAP Signing? (Y/N): '
+    crackmapexec ldap Recon/dcIpAddresses.txt -M ldap-signing
+fi
